d09aa72d42b80616f6f235093ae980d5797f0e6f
jack <275287908@qq.com>
2026-07-03 21:14:33 +0800

    update

--- src/0501-test.js
+++ src/0501-test.js
@@ -4,3 +4,17 @@ var Vehicle = function(){
 
 var v = new Vehicle();
 console.log(v.price);
+
+function Fubar(foo, bar){
+    if (!(this instanceof Fubar)){
+        return new Fubar(foo, bar);
+    }
+    this._foo = foo;
+    this._bar = bar;
+}
+
+var result = Fubar(1,2);
+console.log(result._foo);
+
+
+
