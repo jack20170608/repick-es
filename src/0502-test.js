a85752af67c954a1e27de15c352fb7998ba6e56c
jack <275287908@qq.com>
2026-07-04 17:40:20 +0800

    add

--- src/0502-test.js
+++ src/0502-test.js
@@ -1,47 +1,55 @@
-'use strict'
+"use strict";
 
-var isAFunction = (Object instanceof Function);
+var isAFunction = Object instanceof Function;
 console.log(`Object is a Instance of Function is ${isAFunction}`);
 
 var a = {
-    print : function(){
-        console.log('hello');
-    }
+  print: function (name) {
+    console.log("hello " + name);
+  },
 };
 
 var b = Object.create(a);
 
-console.log(`${Object.getPrototypeOf(a) === Object.prototype}`)
+console.log(`${Object.getPrototypeOf(a) === Object.prototype}`);
 console.log(`${Object.getPrototypeOf(b) === a}`);
 console.log(`${b instanceof Object}`);
+a.print("a");
+b.print("b");
 
-console.log("--==================================================")
-var A = function(id){
-    this.id = id;
-}
+console.log("--==================================================");
+var A = function (id) {
+  this.id = id;
+  this.print = function () {
+    console.log(`XXXXXX ${this.id}`);
+  };
+};
 
-A.prototype.sayHi = function(){
-    console.log(`Hi, my id is ${this.id}`);
-}
+A.prototype.sayHi = function () {
+  console.log(`Hi, my id is ${this.id}`);
+};
 
 var B = Object.create(A);
 
-console.log(`${Object.getPrototypeOf(A) === Function.prototype}`)
+B.prototype = Object.create(A.prototype);
+
+console.log(`${Object.getPrototypeOf(A) === Function.prototype}`);
 console.log(`${Object.getPrototypeOf(B) === A}`);
 console.log(`${B instanceof Object}`);
 console.log(`${B instanceof Function}`);
-console.log(`typeof A is ${typeof A}`)
-console.log(`typeof B is ${typeof B}`)
+console.log(`typeof A is ${typeof A}`);
+console.log(`typeof B is ${typeof B}`);
 
 var a100 = new A(100);
 a100.sayHi();
+a100.print();
 
 Object.setPrototypeOf(B, Function.prototype);
 console.log(`${Object.getPrototypeOf(B) === Function.prototype}`);
 console.log(`After setPrototypeOf, the typeof B is ${typeof B}`);
-console.log(`After setPrototypeOf, instance of Function of B is ${B instanceof Function}`);
+console.log(
+  `After setPrototypeOf, instance of Function of B is ${B instanceof Function}`,
+);
 
 // var b100 = new B(100);
 // b100.sayHi();
-
-
