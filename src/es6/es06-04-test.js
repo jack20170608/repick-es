a9c2b3de667b58a193e107970adf2c8ca78a5d10
jack <275287908@qq.com>
2026-07-08 09:32:30 +0800

    commit

---
+++ src/es6/es06-04-test.js
@@ -0,0 +1,17 @@
+let total = 30;
+let msg = passthru`The total is ${total} (${total*1.05} with tax)`;
+
+function passthru(literals) {
+  let result = '';
+  let i = 0;
+
+  while (i < literals.length) {
+    result += literals[i++];
+    if (i < arguments.length) {
+      result += arguments[i];
+    }
+  }
+
+  return result;
+}
+
