eab58a67986a5a041f1b2b60beed43a0f8eb5dc5
jack <275287908@qq.com>
2026-09-06 21:49:25 +0800

    add

--- src/00-advanced-javascript-programe/chapter4-variable/0403-context.js
+++ src/00-advanced-javascript-programe/chapter4-variable/0403-context.js
@@ -1,8 +1,6 @@
 let color = 'blue';
 
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-async function changeColor() {
+function changeColor() {
   let anotherColor = 'red';
 
   function swapColor() {
@@ -11,17 +9,9 @@ async function changeColor() {
     anotherColor = tempColor;
   }
   swapColor();
-  for(let i=0; i < 20; i++) {
-    console.log("sleep inside.....")
-    await sleep(100);
-  }
 }
 
 changeColor();
 console.log(color);
 
-for(let i=0; i < 20; i++) {
-  console.log("sleep inside.....")
-  await sleep(100);
-}
 
